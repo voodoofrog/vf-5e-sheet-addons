0439d7c512b9d39d94dd01eadfc45131a59bbff2
VoodooFrog <voodoofrog@forgottendream.co.uk>
2024-02-24 17:35:59 +0000

    eslint/prettier config for Svelte

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,12 @@
 module.exports = {
-   semi: true,
-   trailingComma: "none",
-   singleQuote: true,
-   printWidth: 120,
-   tabWidth: 2,
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-svelte'],
+  overrides: [{
+    files: '*.svelte',
+    options: { parser: 'svelte' }
+  }]
 };
