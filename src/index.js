c08288a2d1a0e905585275daab4c4876ffe48fea
VoodooFrog <voodoofrog@forgottendream.co.uk>
2024-02-24 01:29:17 +0000

    Removed some unfinished work

--- src/index.js
+++ src/index.js
@@ -123,12 +123,3 @@ Hooks.on('updateItem', async (item, data) => {
     updateSpellForCharacter(item, data);
   }
 });
-
-Hooks.on('renderItemSheet', (sheet, [html]) => {
-  if (game.user.isGM) return;
-  const unidentified = sheet.item.system.identified === false;
-  if (!unidentified) return;
-  html
-    .querySelectorAll('.dnd5e.sheet.item .sheet-header .item-subtitle label:has(input:not([disabled]))')
-    .forEach((n) => n.remove());
-});
