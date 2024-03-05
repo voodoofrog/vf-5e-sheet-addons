6ee706c4e214e2c0a21aee638365b881921378d3
VoodooFrog <voodoofrog@forgottendream.co.uk>
2024-03-05 12:22:05 +0000

    Add quick setter to app

--- src/applications/SpellbookManagerShell.svelte
+++ src/applications/SpellbookManagerShell.svelte
@@ -1,12 +1,13 @@
 <svelte:options accessors={true} />
 
 <script>
+  import { onDestroy } from 'svelte';
   import { ApplicationShell } from '#runtime/svelte/component/core';
   import { TJSDocument } from '#runtime/svelte/store/fvtt/document';
   import { localize } from '#runtime/svelte/helper';
   import { getValidClasses } from '../spell-preparation';
   import SpellManagementComponent from '../components/SpellManagementComponent.svelte';
-  import { MODULE_ID, SPELL_MANAGER, SETTINGS } from '../constants';
+  import { MODULE_ID, SPELL_MANAGER, SETTINGS, FLAGS } from '../constants';
   import { spellStores } from '../spell-preparation';
 
   export let elementRoot;
@@ -18,14 +19,18 @@
   const spellFilter = (item) => item.type === 'spell';
   const levelFilter = (item) => item.system.level >= minLevel;
   const spellIdFilter = (item) => $spellStore.includes(item.id);
+  const prepFilter = (item) => item.system.preparation.mode === 'prepared';
   const isAddMode = mode === SPELL_MANAGER.MODES.ADD;
   const actorDoc = new TJSDocument(actor);
   const filters = [spellFilter, levelFilter];
   const classes = getValidClasses(actor).map((vc) => vc.name);
   const sourcesEnabled = game.settings.get(MODULE_ID, SETTINGS.USE_CLASS_SOURCES);
+  let updating = false;
 
   if (isAddMode) {
     filters.push(spellIdFilter);
+  } else {
+    filters.push(prepFilter);
   }
 
   const spells = actorDoc.embedded.create(Item, {
@@ -33,10 +38,60 @@
     filters,
     sort: (a, b) => a.system.level - b.system.level || a.name.localeCompare(b.name)
   });
+
+  const onChangeSelect = async (accessor, value) => {
+    updating = true;
+    for (const spell of spells) {
+      await spell.update({ [accessor]: value });
+    }
+    updating = false;
+  };
+
+  onDestroy(() => actorDoc.destroy());
 </script>
 
 <ApplicationShell bind:elementRoot>
   <main>
+    {#if sourcesEnabled || isAddMode}
+      <div class="quick-setter card">
+        <h3 class="title">{localize(`${MODULE_ID}.${SPELL_MANAGER.ID}.headers.quick-set`)}</h3>
+        {#if isAddMode}
+          <div class="spell-prep quick-select">
+            <select
+              name="system.preparation.mode"
+              class="roboto-upper unselect"
+              on:change={(e) => onChangeSelect('system.preparation.mode', e.currentTarget.value)}
+              disabled={updating}
+            >
+              <option value="" disabled selected={!updating}>
+                {localize(`${MODULE_ID}.${SPELL_MANAGER.ID}.quick-set.prep`)}
+              </option>
+              {#each Object.entries(CONFIG.DND5E.spellPreparationModes) as [key, value]}
+                <option value={key}>{value}</option>
+              {/each}
+            </select>
+          </div>
+        {/if}
+        {#if sourcesEnabled}
+          <div class="spell-sources quick-select">
+            <select
+              name="spell-source"
+              class="roboto-upper unselect"
+              on:change={(e) => onChangeSelect(`flags.${MODULE_ID}.${FLAGS.SPELL_SOURCE}`, e.currentTarget.value)}
+              disabled={updating}
+            >
+              <option value="" disabled selected={!updating}>
+                {localize(`${MODULE_ID}.${SPELL_MANAGER.ID}.quick-set.source`)}
+              </option>
+              <option value="">Other</option>
+              {#each classes as c}
+                <option value={c}>{c}</option>
+              {/each}
+            </select>
+          </div>
+        {/if}
+      </div>
+    {/if}
     <div class="items-section card">
       <div class="items-header header">
         <h3 class="item-name spell-header">{localize(`${MODULE_ID}.${SPELL_MANAGER.ID}.headers.name`)}</h3>
@@ -49,15 +104,23 @@
         {/if}
       </div>
       <ol class="item-list unlist">
-        {#each [...$spells] as spell (spell.id)}
-          <SpellManagementComponent {spell} {classes} {mode} />
-        {/each}
+        {#key !updating}
+          {#each [...$spells] as spell (spell.id)}
+            <SpellManagementComponent {spell} {classes} {mode} {updating} />
+          {/each}
+        {/key}
       </ol>
     </div>
   </main>
 </ApplicationShell>
 
 <style lang="scss">
+  @mixin card-style {
+    border-radius: 4px;
+    box-shadow: 0 0 6px var(--dnd5e-shadow-45);
+    border: 1px solid var(--dnd5e-color-gold);
+  }
+
   main {
     text-align: center;
     display: flex;
@@ -66,14 +129,72 @@
 
     --tjs-input-text-width: 100px;
 
+    .quick-setter {
+      position: relative;
+      align-items: stretch;
+      display: flex;
+      margin-bottom: 0.5rem;
+
+      &.card {
+        @include card-style;
+        background: var(--dnd5e-color-iron-gray);
+
+        .title {
+          border: none;
+          margin: 0;
+          padding: 0.5rem;
+          font-family: var(--dnd5e-font-roboto-slab);
+          font-size: var(--font-size-13);
+          font-weight: bold;
+          line-height: 1;
+          text-align: left;
+          flex: 1;
+          color: var(--dnd5e-color-gold);
+        }
+
+        .spell-sources {
+          width: 180px;
+        }
+
+        .spell-prep {
+          width: 155px;
+        }
+
+        .quick-select {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          text-align: center;
+          font-size: var(--font-size-12);
+          padding-top: 2px;
+
+          select.unselect {
+            border: none;
+            padding: 0;
+            background: transparent;
+            height: unset;
+            color: var(--dnd5e-color-gold);
+
+            option {
+              color: var(--color-text-dark-5);
+            }
+          }
+
+          .roboto-upper {
+            font-family: var(--dnd5e-font-roboto);
+            font-weight: bold;
+            text-transform: uppercase;
+          }
+        }
+      }
+    }
+
     .items-section {
       position: relative;
 
       &.card {
-        border-radius: 4px;
+        @include card-style;
         background: var(--dnd5e-color-card);
-        box-shadow: 0 0 6px var(--dnd5e-shadow-45);
-        border: 1px solid var(--dnd5e-color-gold);
 
         .items-header {
           display: flex;
